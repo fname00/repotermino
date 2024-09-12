2340a831bb5507baed26e906bdf87b6d3a7fb6e7
fname00 <toka.poland@gmail.com>
2024-09-12 09:59:18 +0200

    database update

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-module.exports = {
-    i18n: {
-      locales: ['en'], // List all your locales here
-      defaultLocale: 'en', // Default locale
-      localePath: 'public/locales', // Path to your locale files
-    },
-  };
-  
