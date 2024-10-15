012e2b3b013303bc2071481cda445215f27ce12e
fname00 <toka.poland@gmail.com>
2024-10-15 22:48:13 +0200

    ROLLBACK TO PREVIOUS VERSION

--- pages/api/storagecdn.js
+++ pages/api/storagecdn.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
           startsWith: 'https://',
         },
       },
-      take:  parseInt(process.env.CDNTAKE, 300),
+      take:  parseInt(process.env.CDNTAKE, 10),
     });
 
     // Wyświetlenie pobranych listingów do debugowania
