89584951cc9266f2df22bfb5502a6af198b4cb4c
fname00 <toka.poland@gmail.com>
2024-12-02 15:58:05 +0100

    Stripe troubleshooting

--- src/components/property/property-single-style/single-activity/OtherInArea.js
+++ src/components/property/property-single-style/single-activity/OtherInArea.js
@@ -4,6 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 
 // Load Stripe outside the component's render to avoid recreating the Stripe object on every render.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 const BookingSidebar = ({ voucherDetails }) => {
   const handleCheckout = async () => {
