eb7c5fa35a342541e1b98a2e6ae0188cd41e217d
fname00 <toka.poland@gmail.com>
2025-01-27 18:25:28 +0100

    cancel stripe link

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -22,7 +22,7 @@ export default async (req, res) => {
       ],
       mode: 'payment',
       success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${req.headers.origin}/cancel`,
+      cancel_url: `${req.headers.origin}`,
     });
 
     res.status(200).json({ id: session.id });
