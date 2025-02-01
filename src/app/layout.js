33b383080d352872856537fd5f29b6494fb466b4
fname00 <toka.poland@gmail.com>
2025-02-01 14:26:27 +0100

    gtm tag change

--- src/app/layout.js
+++ src/app/layout.js
@@ -52,7 +52,7 @@ export default function RootLayout({ children }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PV822L8');
+            })(window,document,'script','dataLayer','GTM-PV822L8N');
           `
         }} />
       </head>
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
         {/* ✅ Fallback GTM, gdy JS jest wyłączony */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PV822L8"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PV822L8N"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
